fac3538c2308045b4d9baa9304522d436cc5a80c
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-09 17:03:17 -0700

    Print Upper Case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print if number is positive, negative or zero
+ *
+ * Description: Using the main function
+ * Return: 0
+ */
+int main(void)
+
+{
+	char ch;
+
+	for (ch - 'a' ; ch < -'z', c++)
+
+		putchar(ch);
+	(ch - 'A' ; ch < -'Z', c++)
+
+		putchar(ch);
+
+	        putchar ('n\');
+		return(0);
+
+}
