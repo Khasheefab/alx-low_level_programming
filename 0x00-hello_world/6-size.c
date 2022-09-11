ef4f9f4fcc4e4d0ee89b9be12638fc84d2fcd772
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-11 13:00:52 -0700

    Size C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 
+
+
 /**
- * main - print string function
- *
- * Description: Using main function
- * Return: 0
- */
+
+*main - print string in put function
+
+*
+
+* Description: Using main function
+
+* Return: 0
+
+*/
+
 int main(void)
+
 {
-	char c;
-	int i;
-	long li;
-	long long li;
-	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of a int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+char c;
+
+int i;
+
+long li;
+
+long long lli;
+
+float f;
+
+
+
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+printf("Size of a int: %ld byte(s)\n", sizeof(i));
+
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+return (0);
+
 }
-
