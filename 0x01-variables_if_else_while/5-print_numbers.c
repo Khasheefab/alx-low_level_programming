8ffcddbe3f93deeece1803ea106269d5cf6945f3
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-12 19:25:59 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print if positive or negative or zero
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
+
