9f63c758baf5ef9bfeec0cedc78f4514187423ce
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-12 18:26:08 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - print if positive, negative or zero
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
