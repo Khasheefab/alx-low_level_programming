52b29f67dd6c19052772a6764738e0ae6d324709
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-13 20:14:39 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet letters
+ * @c: a character check
+ * Return: returns 0 or 1 dependant on condition
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+
