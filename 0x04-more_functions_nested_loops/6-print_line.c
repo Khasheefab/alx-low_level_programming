2bf0ada754c84ff1e92fa5dae58195626ec374d4
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-15 19:55:01 -0700

    Print lone

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,8 +12,11 @@ void print_line(int n)
 
 	if (n <= 0)
 		putchar('\n');
-	for (x = 0; x <= n; x++)
-		putchar(95);
-	putchar('\n');
+	else
+	{
+		for (x = 0; x <= n; x++)
+			putchar(95);
+		putchar('\n');
+	}
 }
 
