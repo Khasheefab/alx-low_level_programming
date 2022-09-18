5e401efe394e74d0e9bcf4425f4056140b224b64
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-18 08:17:43 -0700

    Main C

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main-Create an Infinite Loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop :(\n'");
+
+			i = 0;
+			/*
+			 * while (i < 10)
+			 * {
+			 * putchar(i);
+			 * }
+			 */
+
+			printf("Infinite loop avoided! \\o/\n");
+
+			return (0);
+}
+
