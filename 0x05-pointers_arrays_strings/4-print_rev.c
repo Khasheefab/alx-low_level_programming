dba58d4e94a74c5b007d50401ccc3e9689311fcc
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-19 15:46:41 -0700

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -> print string in reverse
+ * @s: string to printed in rev
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+			for (i = n - 1; i >= 0; i--)
+			{
+				_putchar(s[i]);
+			}
+	_putchar('\n');
+}
+
+
