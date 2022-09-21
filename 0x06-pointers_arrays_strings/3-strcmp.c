fd4f9e20bf5cea46c5d024ca46399b67f1c02392
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-21 10:24:07 -0700

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> Purposes to Compare
+ * @s1: First Parameter
+ * @s2: Second Parameter
+ * Return: Integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
+
