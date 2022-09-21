4f96101a2e4d3213110291af0ac4e28d8462a71e
Khasheef <khasheef.abrahams@yahoo.com>
2022-09-21 10:48:03 -0700

    Reverse Array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array -> Reversing an Array
+ * @a: Array a
+ * @n: An Element of an array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+		for (i = 0; i < n; i++)
+			p++;
+
+	for (k = 0; k < n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
+
